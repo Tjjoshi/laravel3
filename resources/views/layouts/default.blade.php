a6ad821588f4d41818a78700c789d60df6549473
toral <tj244@njit.edu>
2018-10-20 17:33:31 -0400

    Task:edit default and about page

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,21 @@
 <!doctype html>
 <html lang="en">
-    @include('includes.head')
+
+@include('includes.head')
 <body>
-    @include('includes.menu')
+<div class="container">
+    <header>
+        @include('includes.menu')
+
+    </header>
     <main role="main">
-    @yield('content')
+        @yield('content')
     </main>
     @include('includes.jsfooter')
-    @include('includes.footer')
+    <footer>
+        @include('includes.footer')
+    </footer>
 </body>
+</div>
 
 </html>
