4aef79276784b1588e0c5615baaa6d86d798aa92
toral <tj244@njit.edu>
2018-10-20 17:58:40 -0400

    Task:add submit button in contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -12,9 +12,10 @@
             <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
         </div>
         <div class="form-group">
-            <label for="body">Example textarea</label>
+            <label for="body">Message</label>
             <textarea name="body" class="form-control" id="body" rows="3"></textarea>
         </div>
+        <button type="submit" class="btn btn-primary mb-2">Submit</button>
     </form>
 
 @endsection
