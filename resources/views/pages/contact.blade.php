c178414262c4098fa5f5a65145566954a1ac8938
toral <tj244@njit.edu>
2018-10-20 17:55:02 -0400

    Task:add form in contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,4 +6,15 @@
     <h1>contacts</h1>
     <p class="lead">Please use this form to contact the site owner</p>
 
+    <form>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+        </div>
+        <div class="form-group">
+            <label for="body">Example textarea</label>
+            <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+        </div>
+    </form>
+
 @endsection
