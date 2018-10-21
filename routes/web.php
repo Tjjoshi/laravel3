5aba74e51995db95cd8b4b9c3710026a42bad6f2
toral <tj244@njit.edu>
2018-10-21 16:38:04 -0400

    Task: Edit web.php file

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', 'PageController@home') ->name('home');
 Route::get('/about','PageController@about' )->name('about');
 Route::get('/contact', 'PageController@contact')->name('contact');
+Route::post('/contact', 'PageController@store')->name('contact.store');
+Route::get('/thanks/{name}', 'PageController@thanks')->name('thanks');
